32cb47de8ba619affa9d1fe97a4455bbb4bf23dc
Xertol <chris@benfieldmail.com>
2015-04-14 19:55:47 +0100

    first
    first commit to site

---
+++ index.php
@@ -0,0 +1,85 @@
+<html>
+    <head>
+        
+        <title>MineDuels - Minecraft PvP</title>
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+        
+    </head>
+    
+    <body>
+    
+        <div class="navbar navbar-default navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <a href="../" class="navbar-brand">MineDuels</a>
+          <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#navbar-main">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+        </div>
+        <div class="navbar-collapse collapse" id="navbar-main">
+          <ul class="nav navbar-nav">
+            <li><a href="play">Play</a></li>
+            <li><a href="store">Store</a></li>
+            <li><a href="stats">Stats</a></li>
+            <li><a href="forums">Forums</a></li>
+            <li><a href="help">Help</a></li>
+            </li>
+            </ul>
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="status" target="_blank">Status</a></li>
+          </ul>
+
+        </div>
+      </div>
+    </div>
+    
+
+
+           <div class="container" style="padding-top: 50px;">
+          
+
+	<h1 style="text-align: center;"> MineDuels </h1>
+	<h3 style="text-align: center; margin-top: -5px;"> The coming together of Mining and PvP </h3> 
+	
+	<hr>
+
+	<div class="row">
+
+	<div class="col-md-4"> 
+
+	<h2 style="text-align: center;"> Fun </h2>
+
+	<hr>
+
+	Quisque mollis nulla nec neque volutpat congue sit amet vestibulum diam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam consequat augue varius congue tincidunt. Suspendisse potenti. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut varius luctus ante, quis eleifend justo pretium quis. Vestibulum sagittis id magna at facilisis. Vivamus suscipit erat id eleifend imperdiet. Fusce sed facilisis tortor. Nulla metus nibh, dapibus vel arcu in, faucibus pretium dolor. In.
+
+	</div>
+
+	<div class="col-md-4"> 
+
+	<h2 style="text-align: center;"> Simple </h2>
+
+	<hr>
+
+	Quisque mollis nulla nec neque volutpat congue sit amet vestibulum diam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam consequat augue varius congue tincidunt. Suspendisse potenti. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut varius luctus ante, quis eleifend justo pretium quis. Vestibulum sagittis id magna at facilisis. Vivamus suscipit erat id eleifend imperdiet. Fusce sed facilisis tortor. Nulla metus nibh, dapibus vel arcu in, faucibus pretium dolor. In.
+
+	</div>
+
+	<div class="col-md-4"> 
+
+	<h2 style="text-align: center;"> Fair </h2>
+
+	<hr>
+	
+	Quisque mollis nulla nec neque volutpat congue sit amet vestibulum diam. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam consequat augue varius congue tincidunt. Suspendisse potenti. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut varius luctus ante, quis eleifend justo pretium quis. Vestibulum sagittis id magna at facilisis. Vivamus suscipit erat id eleifend imperdiet. Fusce sed facilisis tortor. Nulla metus nibh, dapibus vel arcu in, faucibus pretium dolor. In.
+
+	</div>
+
+	</div>	
+	
+	</div>
+
+    </body>
+</html>
